eaf11f8295f4c12ab424b9ab058322e22de8b7fc
sibahtu <redietsibahtu@gmail.com>
2023-06-20 07:52:42 -0500

    ready to be deployed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,28 +8,7 @@ const Home = ({ token }) => {
     return (
       <div className='mainpage'>
         <PublicNavbar />
-         <div className='welcomecontainer'>
-             <img
-    //     // //     className='last'
-    //     // //     src='http://cdn.cnn.com/cnnnext/dam/assets/200701111529-08-1980s-fashion-history-restricted.jpg'
-    //     // //   />
-    //     // //   <img
-    //     // //     className='last'
-    //     // //     src='https://www.wonderwall.com/wp-content/uploads/sites/2/2016/09/104-191263_Actual.jpg'
-    //     // //   />
-    //     // //   <img
-    //     // //     className='last'
-    //     // //     src='https://api.time.com/wp-content/uploads/2020/01/GettyImages-104401010.jpg'
-    //     // //   />
-    //     // //   <img
-    //     // //     className='last'
-    //     // //     src='https://nypost.com/wp-content/uploads/sites/2/2022/08/richard-simmons-10-1.jpg'
-    //     // //   />
-    //     // //   <img
-    //     // //     className='top'
-    //     // //     src='https://i.pinimg.com/originals/6a/24/33/6a2433a022b8af86539908bdfe4a985f.jpg'
-             />
-           </div>
+         <div className='welcomecontainer'></div>
          <div id='welcome'>
           <h1 className='welcomeline'>Welcome to</h1>
           <h3 className='fitness'>Fitness Trac.kr</h3>
